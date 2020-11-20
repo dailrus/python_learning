d5048af3d94fae961b28466d8f4bd3d555fa77fe
dailrus <kanadroy761@gmail.com>
2020-11-20 22:58:51 +0300

    Another structure change

--- src/record_urls/get_list_from_record_api.py
+++ src/record_urls/get_list_from_record_api.py
@@ -1,6 +1,8 @@
+import os
 import requests
+
 user_id = 12345
 url = 'http://radiorecord.ru/radioapi/stations'
 answer = requests.get(url)
-with open('output.txt', 'w') as output_file:
+with open(os.path.abspath('files/raw_url.txt'), 'w', encoding='utf-8') as output_file:
     output_file.write(answer.text)
